c303c2f42789cce6dc982f703d8aaeb1a250cf4e
Kushal Likhi <kushal@intelligrape.com>
2014-09-24 13:56:11 +0530

    Added the version number in JAR builds

--- provider/Android/source/build.sh
+++ provider/Android/source/build.sh
@@ -2,7 +2,9 @@ rm -rf ./build
 
 ant -f antfile.xml
 
-cp -rfv build/strap-tizen-SDK-bundle.jar ../strap-tizen-SDK-bundle.jar
+version=`cat VERSION`
 
-cp -rfv build/strap-tizen-SDK-core.jar ../strap-tizen-SDK-core.jar
+cp -rfv build/strap-tizen-SDK-bundle.jar ../strap-tizen-SDK-bundle-$version.jar
+
+cp -rfv build/strap-tizen-SDK-core.jar ../strap-tizen-SDK-core-$version.jar
 
