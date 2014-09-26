eed03b36376a285df56d393dd3a25295a3ac2ea8
rishabhdixit <rishabh.dixit@intelligrape.com>
2014-09-26 18:07:25 +0530

    Changed functionality of non strap related data

--- examples/Android/ocean-survey-demo-Approach1/Android-provider/src/com/straphq/sdk/tizen/example/oceansurvey/OceanSurveyFullyManagedService.java
+++ examples/Android/ocean-survey-demo-Approach1/Android-provider/src/com/straphq/sdk/tizen/example/oceansurvey/OceanSurveyFullyManagedService.java
@@ -1,5 +1,7 @@
 package com.straphq.sdk.tizen.example.oceansurvey;
 
+import java.io.IOException;
+
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
@@ -11,14 +13,24 @@ import com.samsung.android.sdk.accessory.SASocket;
 import com.straphq.sdk.tizen.StrapMetrics;
 import com.straphq.sdk.tizen.dto.StrapMessageDTO;
 import com.straphq.sdk.tizen.exception.StrapSDKException;
+import com.straphq.sdk.tizen.impl.TizenConnectionImpl;
 import com.straphq.sdk.tizen.interfaces.StrapTizenSDKMessageListener;
 
 public class OceanSurveyFullyManagedService extends StrapMetrics {
     public static final String TAG = "OceanSurveyFullyManagedService";
 
-    public static final int SERVICE_CONNECTION_RESULT_OK = 0;
-
     public static final int CHANNEL_ID = 104;
+
+    TizenConnectionImpl.TizenConnectionListener mConnection;
+
+    public static final String SEALION = "SEALION";
+    public static final String DOLPHIN = "DOLPHIN";
+    public static final String PELICAN = "PELICAN";
+
+    int SEALION_COUNT = 0;
+    int DOLPHIN_COUNT = 0;
+    int PELICAN_COUNT = 0;
+
 
 
     private final IBinder mBinder = new LocalBinder();
@@ -36,14 +48,26 @@ public class OceanSurveyFullyManagedService extends StrapMetrics {
     @Override
     protected void onFindPeerAgentResponse(SAPeerAgent saPeerAgent, int i) {
         // TODO Auto-generated method stub
+        Log.d(TAG,"onFindPeerAgentResponse " + i);
 
     }
 
-    @Override
-    protected void onServiceConnectionResponse(SASocket saSocket, int i) {
-        // TODO Auto-generated method stub
+     @Override
+     protected void onServiceConnectionResponse(SASocket thisConnection, int result) {
+            if (result == CONNECTION_SUCCESS) {
+                if (thisConnection != null) {
+                    mConnection = (TizenConnectionImpl.TizenConnectionListener) thisConnection;
+                } else {
+                    Log.e(TAG, "SASocket object is null");
+                }
+            } else if (result == CONNECTION_ALREADY_EXIST) {
+                Log.e(TAG, "onServiceConnectionResponse, CONNECTION_ALREADY_EXIST");
+            } else {
+                Log.e(TAG, "onServiceConnectionResponse result error =" + result);
+            }
 
-    }
+     }
+
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -73,22 +97,43 @@ public class OceanSurveyFullyManagedService extends StrapMetrics {
             addMessageListener(new StrapTizenSDKMessageListener() {
 
                 @Override
-                public void onMessage(StrapMessageDTO strapMessageDTO) {
-                    Log.d("OnMessge", "OnMessge");
-                    processReceivedData(strapMessageDTO);
+                public void onError(StrapSDKException error) {
+                Log.e(TAG, "Connection is not alive ERROR: " + error.getMessage());
 
                 }
 
                 @Override
-                public void onError(StrapSDKException e) {
-                    // TODO Auto-generated method stub
+                public void onConnectionLost(StrapSDKException error) {
+                Log.e(TAG, "Error on onServiceConectionLost " + error.getMessage());
+                mConnection = null;
+                }
 
+                public void onMessage(byte[] data) {
+                String message = new String(data);
+                if(message.equals(SEALION)){
+                    SEALION_COUNT++;
+                    message += SEALION_COUNT;
+                }else {
+                }if(message.equals(DOLPHIN)){
+                DOLPHIN_COUNT++;
+                message += DOLPHIN_COUNT;
+                }else if(message.equals(PELICAN)){
+                PELICAN_COUNT++;
+                message += PELICAN_COUNT;
+                }
+
+                Log.d("ANIMAL_NAME_WITH_COUNT", message);
+                try {
+                mConnection.send(CHANNEL_ID, message.getBytes());
+                } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                }
                 }
 
                 @Override
-                public void onConnectionLost(StrapSDKException e) {
-                    // TODO Auto-generated method stub
-
+                public void onStrapMessage(StrapMessageDTO strapMessageDTO) {
+                processReceivedData(strapMessageDTO);
                 }
             });
     }
